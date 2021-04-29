29e31aeb4740d8eb391f1fb47fe7e5a18e338752
Parthavi <parthavi96@gmail.com>
2021-04-29 15:07:36 -0700

    Included readline module to work on data given by user in console

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-// Logic with static input
-var inp = [3,25,209]
-out = []
+// To read input and print output to console
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
+// After running the file it asks for input to process 
+readline.question('Please enter digits to process', numbers => {
+readline.close();
+
+out = []
 // Building an array with phonetic equivalent of there respective indexes
 var num =  ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
 var temp = 0
 var result = ""
-
+// Spliting the input data with space
+var inp = numbers.split(" ")
 // Calling function for all the input numbers
-inp.forEach(phoneticEqu)
+inp.forEach(myFunction)
 
-function phoneticEqu(i){
+function myFunction(i){
+    // Since input from console is in string need to convert it into integer
     i = parseInt(i)
     // Separating each digits in number
     while(i>0){
@@ -25,6 +34,7 @@ function phoneticEqu(i){
     }
     result += ","
 }
- 
+// To write the output in console
 console.log(result)
 
+});
