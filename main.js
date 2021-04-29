c6ec98d0a0163be2403e05500f83d59c9e57c216
Parthavi <parthavi96@gmail.com>
2021-04-29 15:44:37 -0700

    Restructuring the code and Resolving the comma issue : a coma was getting appended at the end

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+// Building an array with phonetic equivalent of there respective indexes
+var num =  ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
+var temp = 0
+var result = ""
+var output =[]
+var out = []
+
+
 // To read input and print output to console
 const readline = require('readline').createInterface({
     input: process.stdin,
@@ -8,17 +16,19 @@ const readline = require('readline').createInterface({
 readline.question('Please enter digits to process', numbers => {
 readline.close();
 
-out = []
-// Building an array with phonetic equivalent of there respective indexes
-var num =  ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
-var temp = 0
-var result = ""
 // Spliting the input data with space
 var inp = numbers.split(" ")
 // Calling function for all the input numbers
-inp.forEach(myFunction)
+inp.forEach(phoneticEqu)
 
-function myFunction(i){
+// To write the output in console
+console.log(output.join(','))
+
+});
+
+
+
+function phoneticEqu(i){
     // Since input from console is in string need to convert it into integer
     i = parseInt(i)
     // Separating each digits in number
@@ -28,13 +38,10 @@ function myFunction(i){
         i = parseInt(i / 10)
     }
     // To get the digits in correct sequence
+    result = ""
     while(out.length >0)
     {
         result += out.pop()
     }
-    result += ","
+    output.push(result)
 }
-// To write the output in console
-console.log(result)
-
-});
